f27bf2a11c2c9d8133ca663de6dbd02fdcdc6783
anricoj1 <anricoj1@southernct.edu>
2019-07-16 15:11:50 -0400

    new pages for api linking

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,33 @@ module.exports = function(app, passport){
     app.get('/auth/google/callback',
         passport.authenticate('google', { successRedirect: '/profile',
                                          failureRedirect: '/'}));
+
+    app.get('/twitter', function(req, res){
+      res.render('../views/pages/twitter/index.ejs');
+    });
+
+    app.get('/facebook', function(req, res){
+      res.render('../views/pages/facebook/index.ejs')
+    });
+
+    app.get('/twitch', function(req, res){
+      res.render('../views/pages/twitch/index.ejs')
+    });
+
+
+    app.get('/youtube', function(req, res){
+      res.render('../views/pages/youtube/index.ejs')
+    });
+
+
+    app.get('/github', function(req, res){
+      res.render('../views/pages/github/index.ejs')
+    });
+
+
+    app.get('/instagram', function(req, res){
+      res.render('../views/pages/instagram/index.ejs')
+    });
 };
 
 function isLoggedIn(req, res, next) {
